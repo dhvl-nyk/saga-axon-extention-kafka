5d72a798b877f282d0ef1d3f15fade8b98f687c6
dhvl-nyk <diablo82.26@gmail.com>
2020-12-28 16:25:39 +0530

    adding payment-service

---
+++ payment-service/src/main/java/paymentservice/aggregates/InvoiceAggregate.java
@@ -0,0 +1,35 @@
+package paymentservice.aggregates;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import progressivecoder.ecommerce.commands.CreateInvoiceCommand;
+import progressivecoder.ecommerce.events.InvoiceCreatedEvent;
+
+@Aggregate
+public class InvoiceAggregate {
+
+    @AggregateIdentifier
+    private String paymentId;
+
+    private String orderId;
+
+    private InvoiceStatus invoiceStatus;
+
+    public InvoiceAggregate() {
+    }
+
+    @CommandHandler
+    public InvoiceAggregate(CreateInvoiceCommand createInvoiceCommand){
+        AggregateLifecycle.apply(new InvoiceCreatedEvent(createInvoiceCommand.paymentId, createInvoiceCommand.orderId));
+    }
+
+    @EventSourcingHandler
+    protected void on(InvoiceCreatedEvent invoiceCreatedEvent){
+        this.paymentId = invoiceCreatedEvent.paymentId;
+        this.orderId = invoiceCreatedEvent.orderId;
+        this.invoiceStatus = InvoiceStatus.PAID;
+    }
+}
---
+++ payment-service/src/main/java/paymentservice/aggregates/InvoiceStatus.java
@@ -0,0 +1,6 @@
+package paymentservice.aggregates;
+
+public enum InvoiceStatus {
+
+    PAID, PAYMENT_REVERSED
+}
